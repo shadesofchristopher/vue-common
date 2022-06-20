43cb911e887f91ec95d21380065c9fc92a354de4
Christopher <105583070+shadesofchristopher@users.noreply.github.com>
2022-06-20 12:21:15 -0400

    Delete vue-common directory

--- vue-common/components/forms/SubmitButton.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-button(type="submit"): slot Submit
-</template>
-
-<style lang="scss" scoped>
-button {
-  @include button;
-} 
-</style>
--- vue-common/mixins/index.js
+++
@@ -1,1 +0,0 @@
-export { default as formMixin } from './form'
