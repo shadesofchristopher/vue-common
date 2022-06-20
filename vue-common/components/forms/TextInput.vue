899ee2b61b64e3e1a6846a49a5d90ed3efae9270
Christopher <105583070+shadesofchristopher@users.noreply.github.com>
2022-06-20 12:20:52 -0400

    Add files via upload

---
+++ vue-common/components/forms/TextInput.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+FormElement(:label="label" :caption="caption")
+  input(
+    ref="input"
+    :type="password ? 'password' : 'text'" 
+    :value="modelValue" 
+    :placeholder="placeholder" 
+    :name="name"
+    @input="$emit('update:modelValue', $event.target.value)"
+  )
+</template>
+
+<script>
+import mixin from '../../mixins/form-element'
+
+export default {
+  mixins: [mixin],
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    password: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted () {
+    if (this.autofocus) {
+      this.$refs.input.focus()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+input {
+  @include form-control;
+}
+</style>
